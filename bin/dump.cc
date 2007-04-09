70bb5b096a409fc78d3add6bdd6751fd55ea2834
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2007-04-09 20:01:34 +0000

    first working version

--- bin/dump.cc
+++ bin/dump.cc
@@ -1,6 +1,7 @@
 //<<<<<< INCLUDES                                                       >>>>>>
 
 #include "FWCore/PluginManager/interface/PluginManager.h"
+#include "FWCore/PluginManager/interface/standard.h"
 #include "FWCore/Utilities/interface/Exception.h"
 
 #include <iostream>
@@ -12,12 +13,38 @@
 using namespace edmplugin;
 
 
-int main (int, char **argv)
+int main (int argc, char **argv)
 {
   int returnValue = EXIT_SUCCESS;
   try {
+    if (argc == 1) {
+      //dump all know plugins
+      PluginManager::configure(standard::config());
+      
+      typedef edmplugin::PluginManager::CategoryToInfos CatToInfos;
+
+      const CatToInfos& catToInfos = edmplugin::PluginManager::get()->categoryToInfos();
+      // map every module to its library.  Code copied from SealPluginDump
+      for (CatToInfos::const_iterator it = catToInfos.begin(), itEnd=catToInfos.end();
+           it != itEnd; ++it)
+      {
+        std::cout <<"Category "<<it->first<<":"<<std::endl;
+        std::string prevPluginName;
+        for (edmplugin::PluginManager::Infos::const_iterator itInfo = it->second.begin(), itInfoEnd = it->second.end(); 
+             itInfo != itInfoEnd; ++itInfo)
+        {
+          std::string pluginName = itInfo->name_;
+          if(pluginName != prevPluginName) {
+            std::cout <<"  "<<pluginName << std::endl;
+            prevPluginName=pluginName;
+          }
+        }
+      }
+    } else {
+      std::cerr <<"no arguments allowed"<<std::endl;
+    }
   }catch(std::exception& iException) {
-    std::cout <<"Caught exception "<<iException.what()<<std::endl;
+    std::cerr <<"Caught exception "<<iException.what()<<std::endl;
     returnValue = 1;
   }
 
